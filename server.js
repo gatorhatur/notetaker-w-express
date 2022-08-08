c299389986a1ef020f23e613b2f114ed044e86eb
Brandon Davis <brandon.davis@conehealth.com>
2022-08-08 10:33:09 -0400

    setting up express base for server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
-const {data} = require('./db/db.json')
+const { data } = require('./db/db.json')
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001
 
 const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-
-
-
-
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`Api Server now listening on port 3001`);
 })
