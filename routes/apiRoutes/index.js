1adfdedaab98045d7aa29efa7c84116b9d0aa131
Brandon Davis <brandon.davis@conehealth.com>
2022-08-09 08:37:19 -0400

    create api get and post routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router()
+const { notes } = require('../../db/db.json');
 
 
+router.get('/notes', (req, res) =>{
+    res.send("Notes is working")
+})
 
+router.post('/notes', (req, res) => {
+    res.send("Note creation is working")
+})
 
 module.exports = router;
